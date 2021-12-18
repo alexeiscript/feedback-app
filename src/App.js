e43e953e42eb5ca7fa0721e8dff2ff028a515688
alexeiscript <suzakukururug1@yandex.com>
2021-12-18 17:57:32 +0200

    create header component, add styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from './components/Header'
 
 function App() {
-  return <div>hello</div>
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
 
 export default App
