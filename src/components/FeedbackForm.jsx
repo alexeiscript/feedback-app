0d7360f04db51508d4e76082d7a95989daa1bf5b
alexeiscript <suzakukururug1@yandex.com>
2021-12-21 16:42:49 +0200

    create rating component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 
 function FeedbackForm() {
   const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [message, setMessage] = useState('')
 
   const handleTextChange = (content) => {
     if (text === '') {
+      setBtnDisabled(true)
       setMessage(null)
     } else if (text !== '' && text.trim().length < 9) {
+      setBtnDisabled(true)
       setMessage('Review must contain at least 10 characters')
     } else {
       setMessage(null)
@@ -20,10 +24,23 @@ function FeedbackForm() {
     setText(content.target.value)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 9) {
+      const newFeedback = {
+        text,
+        rating,
+      }
+
+      console.log(newFeedback)
+    }
+  }
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your experience with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className='input-group'>
           <input
             onChange={handleTextChange}
